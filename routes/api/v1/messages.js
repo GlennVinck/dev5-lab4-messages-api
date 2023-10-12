a0026176ac57f586fab9df2d801dea366d4ab892
GlennVinck <vinck_glenn@hotmail.com>
2023-10-12 18:27:14 +0200

    feat: add put and delete routes

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -7,26 +7,60 @@ router.get("/", (req, res) => {
     res.json({
         status: 'success',
         message: 'GET messages',
-    //    data:[
-    //     {
-    //         id: 1,
-    //         name: 'John Doe',
-    //         message: 'Hello World',
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'John Doe',
-    //         message: 'Hello World 2',
-    //     }
-    // ]
+        data:[
+        {
+            id: 1,
+            name: 'John Doe',
+            message: 'Hello World',
+        },
+        {
+            id: 2,
+            name: 'John Doe',
+            message: 'Hello World 2',
+        }
+    ]
     })
  })
+
+
+router.get("/:id", (req, res) => {
+        let message = req.params.id;
+    res.json({
+        status: 'success',
+        message: `GET message with id ${message}`,
+    })    
+     })
+
+// router.get("/?user=username", (req, res) => {
+//     let message = req.params.username;
+//     res.json({
+//         status: 'success',
+//         message: `GET message with username ${message}`,
+//     })
+//     })
 
  router.post("/", (req, res) => {
     let message = req.body.message;
     res.json({
         status: 'success',
         message: `POST ${message}`,
+    })
+ })
+
+ router.put("/:id", (req, res) => {
+    let message = req.params.id;
+    res.json({
+        status: 'success',
+        message: `UPDATING a message with id ${message}`,
+    })
+ })
+
+
+ router.delete("/:id", (req, res) => {
+    let message = req.params.id;
+    res.json({
+        status: 'success',
+        message: `DELETING a message with id ${message}`,
     })
  })
 
