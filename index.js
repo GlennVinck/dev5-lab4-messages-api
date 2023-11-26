be3a1f837fde811490659a1757bd89ef80c096fc
glennvinck <vinck_glenn@hotmail.com>
2023-11-27 00:58:05 +0100

    feat: enable CORS

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// enable CORS
+const cors = require("cors");
 
 //include routes
 //const logger = require('./middleware/logger');
-const messagesRouter = require('./routes/api/v1/messages.js');
+const messagesRouter = require("./routes/api/v1/messages.js");
 
 //json body parser
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/messages", messagesRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
